4e6007b1f5573ab3a4e6ba15a14e8b324954b26c
Patrick Brett <pkbrett37@gmail.com>
2019-11-13 19:37:33 +1100

    Update style

--- public/js/fetch-counts.js
+++ public/js/fetch-counts.js
@@ -1,5 +1,5 @@
 fetch('https://ecsptkcc68.execute-api.us-east-1.amazonaws.com/test').then(res => {
     res.text().then(text => {
-        document.getElementById('services-count').innerHTML = text;
+        document.getElementById('services-count').innerHTML = "<div id='count-number' title=\"Don't say I didn't warn you.\"><a href='https://aws.amazon.com' target='_blank'>" + text + "</a></div>";
     })
 });
